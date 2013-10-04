c85d4edcb1b3855ec5ed8f3cfd299cd97811cf44
Petter Nyman <petter@pttr.se>
2013-10-04 04:37:25 +0200

    compass view pretty much rewritten, different color for text, allow for avatars

--- app/scripts/views/compass-view.js
+++ app/scripts/views/compass-view.js
@@ -30,7 +30,7 @@ define([
             var image = new Image();
             image.src = 'public/images/compass.png';
             image.onload = function(){
-                _this.context.drawImage(image,0,0,300,300);
+                _this.context.drawImage(image,0,0,290,290);
                 callback();
                 return _this;
             };
@@ -38,13 +38,30 @@ define([
 
         draw2DPoint: function(name,x,y){
             var _this = this;
-            _this.context.fillStyle = 'rgb(0,0,0)';
+            _this.context.save();
+            var randomcolor = Math.floor(Math.random()*150);
+            _this.context.fillStyle = 'rgb('+Math.floor(26+randomcolor/6)+','+randomcolor+','+(randomcolor+32)+')';
+            console.log(_this.context.fillStyle);
+            _this.context.font = '16px Helvetica Neue,Helvetica,Arial Black';
             _this.context.fillText(name,x,y);
+            _this.context.restore();
             return _this;
+        },
+
+        drawImage: function(imageurl,x,y){
+            var _this = this;
+            var image = new Image();
+            image.src = imageurl;
+            image.onload = function(){
+                _this.context.drawImage(image,x,y,30,30);
+                return _this;
+            };
+
         },
 
         render: function(){
             var _this = this;
+            var compassoffset = 30;
 
             var context = {
             };
@@ -55,12 +72,17 @@ define([
             _this.context = _this.canvas.getContext('2d');
             _this.draw2DCompass(function(){
                 var canvasHeight = _this.canvas.height;
+                //var canvasWidth = $(_this.canvas).width();
 
-                _.each(_this.model.get('completed'),function(completed){
-                    var canvasX = completed.result.x*6;
-                    var canvasY = completed.result.y*6;
+                _.each(_this.model.get('completed').toJSON(),function(completed){
                     window.test = _this;
-                    _this.draw2DPoint(completed.name,canvasX,canvasHeight-canvasY);
+                    var canvasX = (completed.result.x*8)+compassoffset;
+                    var canvasY = (completed.result.y*8)+compassoffset;
+                    if(completed.imageurl){
+                        _this.drawImage(completed.imageurl,canvasX,canvasHeight-canvasY);
+                    } else {
+                        _this.draw2DPoint(completed.name,canvasX,canvasHeight-canvasY);
+                    }
                 });
             });
         }
